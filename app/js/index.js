c47038d18991c09bb7b5242604215ba27e89ae3f
hebye <haha@163.com>
2016-07-05 22:56:10 +0800

    update

--- app/js/index.js
+++ app/js/index.js
@@ -1,23 +1,7 @@
-
+"use strict";
 angular.module('notesApp',[]).controller('mainCtrl',[function(){
 	var self=this;
-	this.person=[{
-			name:"tom",
-			age:18,
-			status:false
-		},{
-			name:"kitty",
-			age:15,
-			status:true
-		}];
-	self.myEvent=function(){
-		console.info(arguments);
-		console.info(this);
-	}
-	self.getClassName=function(status){
-		return{
-			show:status,
-			hide:!status,
-		}
+	self.submit=function(){
+		console.info(self.user)
 	}
 }]);
