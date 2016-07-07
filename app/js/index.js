b02a85c56d0fd98f268d561963577950d4b9a281
马若涵 <marh@zhong-ying.com>
2016-07-07 15:48:15 +0800

    update

--- app/js/index.js
+++ app/js/index.js
@@ -21,7 +21,7 @@
 		{value:'3',key:'33',type:'even'},
 	]
 }]);*/
-angular.module('notesApp',[]).controller('MainCtrl',['$log','$location',function($log,$x){
+/*angular.module('notesApp',[]).controller('MainCtrl',['$log','$location',function($log,$x){
 	var self=this;
 	self.printLog=function(){
 		$log.log($x);
@@ -29,4 +29,53 @@ angular.module('notesApp',[]).controller('MainCtrl',['$log','$location',function
 		$log.error('this is error!')
 		$log.debug(this)
 	};
-}]);
+}]);*/
+angular.module('notesApp', [])
+    .controller('MainCtrl', [function() {
+        var self = this;
+        self.tab = 'first';
+        self.open = function(tab) {
+            self.tab = tab;
+        };
+    }])
+    .controller('SubCtrl', ['ItemService', function(ItemService) {
+        var self = this;
+        self.list = function() {
+            return ItemService.list();
+        };
+        self.add = function() {
+            /*ItemService.add({
+            	id:self.list().length+1,
+            	label:self.list().length
+            });*/
+    		ItemService.callBack().then(function(data){
+    			console.info(data)
+    		},function(data){
+    			alert(data)
+    		})
+        };
+    }])
+    .service('ItemService', ['$q', '$http', function($q, $http) {
+        var items = [
+            { id: 1, label: 'item 0' },
+            { id: 2, label: 'item 1' },
+        ];
+        this.callBack=function(){
+        	var deferred = $q.defer();
+        	$http({
+        		method:'get',
+        		url:'http://127.0.0.1:8080/app',
+        	}).success(function(data){
+        		deferred.resolve(data);
+        	}).error(function(){
+        		deferred.reject('error')
+        	});
+        	return deferred.promise;
+        }
+        this.list = function() {
+            return items;
+        }
+        this.add = function(item) {
+            items.push(item);
+        }
+    }]);
